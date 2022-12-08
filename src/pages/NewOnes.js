fa18882902b7acdb38f451fd6b65f2441327265a
Shamim <mim24sd@gmail.com>
2022-12-08 11:56:11 +0330

    add folders and styles

--- src/pages/NewOnes.js
+++
@@ -1,5 +0,0 @@
-const NewOnes = () => {
-  return <div></div>;
-};
-
-export default NewOnes;
---
+++ src/pages/newAlbumsAndSingles/NewAlbumsAndSingles.js
@@ -0,0 +1,5 @@
+const NewAlbumsAndSingles = () => {
+  return <div></div>;
+};
+
+export default NewAlbumsAndSingles;
