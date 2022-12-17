400a69f0e5313d96c5313d4bbe10ad8e81331369
Shamim <mim24sd@gmail.com>
2022-12-17 20:49:50 +0330

    add better semantics

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,17 +2,19 @@ import "./styles.css";
 
 const Sidebar = () => {
   return (
-    <ul className="sidebar">
-      <li>
-        <a>Playlists</a>
-      </li>
-      <li>
-        <a>Singles</a>
-      </li>
-      <li>
-        <a>Albums</a>
-      </li>
-    </ul>
+    <nav className="container">
+      <ul className="sidebar">
+        <li>
+          <a>Playlists</a>
+        </li>
+        <li>
+          <a>Singles</a>
+        </li>
+        <li>
+          <a>Albums</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
