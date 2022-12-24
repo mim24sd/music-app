fe1c244087105e26161787dcc579573b3b39439c
Shamim <mim24sd@gmail.com>
2022-12-24 11:29:54 +0330

    add pages in one page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Singles from "./pages/Singles";
-import Albums from "./pages/Albums";
-import PlayLists from "./pages/PlayLists";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/singles" element={<Singles />} />
-        <Route path="/albums" element={<Albums />} />
-        <Route path="/playlists" element={<PlayLists />} />
+        <Route path="/" element={<Home title="New albums and singles" />} />
+        <Route
+          path="/singles"
+          element={<Home content={"single"} title="Singles" />}
+        />
+        <Route
+          path="/albums"
+          element={<Home content={"album"} title="Albums" />}
+        />
+        <Route
+          path="/playlists"
+          element={<Home content={"playlists"} title="Playlists" />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,25 @@
 import Header from "../../components/Header";
 import MusicList from "../../components/MusicList";
+import PlayLists from "../../components/PlayLists";
 import Sidebar from "../../components/Sidebar";
 
 import "./styles.css";
 
-const Home = () => {
+const Home = ({ content, title }) => {
+  let contentElement = <MusicList />;
+  if (content === "album") {
+    contentElement = <MusicList type={content} />;
+  } else if (content === "single") {
+    contentElement = <MusicList type={content} />;
+  } else if (content === "playlists") {
+    contentElement = <PlayLists />;
+  }
+
   return (
     <main className="main">
       <Sidebar />
-      <Header title="New albums and singles" />
-      <MusicList />
+      <Header title={title} />
+      {contentElement}
     </main>
   );
 };
